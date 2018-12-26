3e384fb7bf97541b3a54c855cb251b503b1c77bc
as28wima@studserv.uni-leipzig.de <abdulnaser66685@gmail.com>
2018-12-26 15:43:45 +0100

    here i added alot of things ,you can check them out.

--- app/src/main/java/com/ak18bj/bj/MainActivity.java
+++ app/src/main/java/com/ak18bj/bj/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ak18bj.bj;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,16 +10,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button loginB;
+    TextView registerhere;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        loginB=(Button) findViewById(R.id.BLogin);
+        registerhere=(TextView)findViewById(R.id.Registerhere);
+        Activity DailyLogActivity= new DailyLogActivity();
+        Activity RegisterActivity= new registerActivity();
+        Onclick(loginB, DailyLogActivity);
+        Onclick(registerhere,RegisterActivity);
+
+
+
+
+
     }
 
     @Override
@@ -35,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    // @Override
+   /*  public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -48,5 +61,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }*/
+
+    public void Onclick(Button b, final Activity activity)
+    {
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(activity);
+            }
+        });
     }
+    public void Onclick(TextView tv,final Activity activity)
+    {
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(activity);
+            }
+        });
+    }
+
+    public void openActivity(Activity activity)
+    {
+        Intent intent=new Intent(this,activity.getClass());
+        startActivity(intent);
+    }
+
+
+
+
+
 }
